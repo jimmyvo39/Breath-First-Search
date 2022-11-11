36e575b81898b0ce4087a2da7685147c72cd8001
Jimmy Vo <j.vo718@gmail.com>
2022-11-11 18:02:53 -0500

    timing class

---
+++ src/scripts/timing.js
@@ -0,0 +1,16 @@
+class Timing {
+    constructor(inhale = 4000,hold = 2000,exhale = 3000){
+        this.inhale = inhale;
+        this.hold = hold;
+        this.exhale = exhale
+
+        // this.inhale.addEventListener("keydown", this.setInhale(this))
+    }
+
+    // setInhale(){
+    //     this.inhale = 
+    // }
+
+    // figure out how to get the duration of holddown
+
+}
