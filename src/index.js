832d4a26dbb074f2ee6fad7ace67ade877584eef
Jimmy Vo <j.vo718@gmail.com>
2022-11-14 17:31:03 -0500

    anime js test

--- src/index.js
+++ src/index.js
@@ -1,44 +1,96 @@
-import Canvas from "./scripts/canvas";
+import anime from 'animejs/lib/anime.es.js';
+
+anime({
+    targets: '#obj',
+    scale: 20,
+    backgroundColor: '#FFF',
+    duration: 3000,
+    loop: true,
+    easing: 'linear'
+});
 
 
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 
-const circle = {
-    x: 300,
-    y: 300,
-    radius: 10,
-    dr: 5 
-    // change in radius
-}
+
+
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+// const circle = {
+//     x: 300,
+//     y: 300,
+//     radius: 80,
+//     dr: 2
+//     // change in radius
+// }
     
-function drawCircle(){
-    ctx.beginPath();
-    ctx.arc(circle.x,circle.y,circle.radius,0,Math.PI*2);
-    ctx.stroke();
-}
+// function drawCircle(){
+//     ctx.beginPath();
+//     ctx.arc(circle.x,circle.y,circle.radius,0,Math.PI*2);
+//     ctx.stroke();
+// }
 
 
-function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+// function inhale() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawCircle();
+//   drawCircle();
 
-  // change size
-  circle.radius += circle.dr
+//   ctx.font = '48px serif';
+//     ctx.textAlign = 'center'
+//     ctx.fillText('INHALE', 300, 300);
+
+//   // change size
+//   circle.radius += circle.dr
  
+//   if (circle.radius >= 280) {
+//     circle.radius = 80;
+//   }
 
-  
-  if (circle.radius >= 280) {
-    circle.dr *= -1;
-  }
 
-  if (circle.radius <= 0) {
-    circle.dr *= -1;
-  }
+//   setInterval(inhale,500);
+// }
 
-  requestAnimationFrame(update);
-}
 
-update();
+// function hold(){
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     circle.radius = 280
+//     drawCircle();
+    
+//     ctx.font = '48px serif';
+//     ctx.textAlign = 'center'
+//     ctx.fillText('HOLD', 300, 300);
+    
+// }
+
+
+// function exhale(){
+    
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     drawCircle();
+
+//     ctx.font = '48px serif';
+//     ctx.textAlign = 'center';
+//     ctx.fillText('EXHALE', 300, 300);
+
+//     // change size
+//     circle.radius = circle.radius - circle.dr
+
+//     if (circle.radius <= 80) {
+//         circle.radius = 280;
+//     }
+
+
+//     setInterval(exhale,2000);
+    
+// }
+
+// function breath() {
+    
+//     inhale();
+//     hold();
+//     exhale();
+
+// }
