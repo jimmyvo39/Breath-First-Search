668f0f3c041d2c8b7aa26e26d85b325a99f8ec16
Jimmy Vo <j.vo718@gmail.com>
2022-11-14 17:51:50 -0500

    better timing

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const ctx = canvas.getContext("2d");
 const circle = {
     x: 300,
     y: 300,
-    radius: 10,
+    radius: 20,
     dr: 5 
     // change in radius
 }
@@ -21,9 +21,11 @@ function drawCircle(){
 
 
 function update() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+console.log(circle.radius)
   drawCircle();
+  
 
   // change size
   circle.radius += circle.dr
@@ -34,11 +36,12 @@ function update() {
     circle.dr *= -1;
   }
 
-  if (circle.radius <= 0) {
+  if (circle.radius <= 20) {
     circle.dr *= -1;
+   
   }
 
-  requestAnimationFrame(update);
 }
+setInterval(update,100);
 
-update();
+// update();
