89f516158c4268babb51c8762b21721de84f218c
jimmyvo39 <84362272+jimmyvo39@users.noreply.github.com>
2023-01-28 14:41:54 -0600

    working modal

--- src/index.js
+++ src/index.js
@@ -23,6 +23,22 @@ document.getElementById("hold").addEventListener("mouseup", function(event) {
 //   console.log(interval)
 });
 
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
 
 
 
